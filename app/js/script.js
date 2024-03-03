affbcbf1e4b652b9befc93e3947b4902ecc864c5
Megha <megha.singh.0272@gmail.com>
2024-03-03 13:55:45 +0530

    nav featues

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,8 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
 
 // hamburger animation menu
 btnHamburger.addEventListener('click', function()
@@ -9,16 +11,25 @@ btnHamburger.addEventListener('click', function()
 
     if(header.classList.contains('open')) //close hamburger menu
     {
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElements.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+
+        })
 
     }
     else //open hamburger menu
     {
+    body.classList.add('noscroll');
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    fadeElements.forEach(function(element){
+
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
+    })
+    
     }
 }
 );
