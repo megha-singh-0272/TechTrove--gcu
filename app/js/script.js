eca7cbe534588b2e19c3adc9ee5ec018be2f2737
Megha <megha.singh.0272@gmail.com>
2024-03-13 11:20:59 +0530

    modification of javascript file

--- app/js/script.js
+++ app/js/script.js
@@ -1,58 +1,70 @@
-const btnHamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header');
-const body = document.querySelector('body');
-const overlay = document.querySelector('.overlay');
-const fadeElements = document.querySelectorAll('.has-fade');
+document.addEventListener('DOMContentLoaded', function () {
+    const btnHamburger = document.querySelector('#btnHamburger');
+    const header = document.querySelector('.header');
+    const body = document.querySelector('body');
+    const fadeElements = document.querySelectorAll('.has-fade');
+    const mobileMenuLinks = document.querySelectorAll('.header_mobile_menu a');
 
-// hamburger animation menu
-btnHamburger.addEventListener('click', function()
-{
-    console.log("click hamburger");
 
-    if(header.classList.contains('open')) //close hamburger menu
-    {
-        body.classList.remove('noscroll');
-        header.classList.remove('open');
-        fadeElements.forEach(function(element){
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
+    btnHamburger.addEventListener('click', function () {
+        console.log("Click hamburger");
 
-        });
-
-    }
-    else //open hamburger menu
-    {
-    body.classList.add('noscroll');
-    header.classList.add('open');
-    fadeElements.forEach(function(element){
-
-        element.classList.remove('fade-out');
-        element.classList.add('fade-in');
+        if (header.classList.contains('open')) {
+            body.classList.remove('noscroll');
+            header.classList.remove('open');
+            fadeElements.forEach(function (element) {
+                element.classList.remove('fade-in');
+                element.classList.add('fade-out');
+            });
+        } else {
+            body.classList.add('noscroll');
+            header.classList.add('open');
+            fadeElements.forEach(function (element) {
+                element.classList.remove('fade-out');
+                element.classList.add('fade-in');
+            });
+        }
     });
+
     
-    }
-}
-);
+    mobileMenuLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            body.classList.remove('noscroll');
+            header.classList.remove('open');
+            fadeElements.forEach(function (element) {
+                element.classList.remove('fade-in');
+                element.classList.add('fade-out');
+            });
+        });
+    });
 
+   
+    let slideIndex = 0;
 
-//gallery automatic slide 
-var slideIndex = 0;
+   
     showSlides();
 
     function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        for (i = 0; i < slides.length; i++) {
+        const slides = document.getElementsByClassName("mySlides");
+        const dots = document.getElementsByClassName("dot");
+
+        for (let i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
+
         slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}
-        for (i = 0; i < dots.length; i++) {
+
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        for (let i = 0; i < dots.length; i++) {
             dots[i].className = dots[i].className.replace(" active", "");
         }
-        slides[slideIndex-1].style.display = "block";
-        dots[slideIndex-1].className += " active";
+
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+
         setTimeout(showSlides, 3000); // Change image every 3 seconds
     }
 
@@ -63,3 +75,5 @@ var slideIndex = 0;
     function currentSlide(n) {
         showSlides(slideIndex = n);
     }
+});
+
